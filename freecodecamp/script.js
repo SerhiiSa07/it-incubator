361773a3ae47f7176fe50a1009da1bc65ee28e50
Serhii <875dfsb6435278785@gmail.com>
2023-03-10 12:19:40 +0100

    it-incubator

--- freecodecamp/script.js
+++ freecodecamp/script.js
@@ -47,3 +47,15 @@ let timRegex = /Tim{4}ber/g; // Change this line
 let result = timRegex.test(timStr);
 
 //9
+
+let favWord = 'favorite';
+let favRegex = /favou?rite/; // Change this line
+let result = favRegex.test(favWord);
+
+//10
+
+let sampleWord = 'astronaut';
+let pwRegex = /(?=\w{5,})(?=\D\w*\d{2})/; // Change this line
+let result = pwRegex.test(sampleWord);
+
+//11
