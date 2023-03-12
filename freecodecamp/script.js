0e5f6d78cef0f95b31b0a697b5ff5687a4bef516
Serhii <875dfsb6435278785@gmail.com>
2023-03-12 15:43:37 +0100

    it-incubator

--- freecodecamp/script.js
+++ freecodecamp/script.js
@@ -97,3 +97,83 @@ console.log(`Net working capital is: ${netWorkingCapital}`);
 let myArray = [1, 2, 3];
 let arraySum = myArray.reduce((previous, current) => previous + current);
 console.log(`Sum of array values is: ${arraySum}`);
+
+//16
+
+let x = 7;
+let y = 9;
+let result = 'to come';
+
+if (x == y) {
+  result = 'Equal!';
+} else {
+  result = 'Not equal!';
+}
+
+console.log(result);
+
+//17
+
+function getNine() {
+  let x = 6;
+  let y = 3;
+  return x + y;
+}
+
+let result = getNine();
+console.log(getNine);
+
+//18
+
+function raiseToPower(b, e) {
+  return Math.pow(b, e);
+}
+
+let base = 2;
+let exp = 3;
+let power = raiseToPower(base, exp);
+console.log(power);
+
+//19
+
+function countToFive() {
+  let firstFive = '12345';
+  let len = firstFive.length;
+  // Only change code below this line
+  for (let i = 0; i < len; i++) {
+    // Only change code above this line
+    console.log(firstFive[i]);
+  }
+}
+
+countToFive();
+
+//20
+
+function zeroArray(m, n) {
+  // Creates a 2-D array with m rows and n columns of zeroes
+  let newArray = [];
+  let row = [];
+  for (let i = 0; i < m; i++) {
+    // Adds the m-th row into newArray
+    row = [];
+    for (let j = 0; j < n; j++) {
+      // Pushes n zeroes into the current row to create the columns
+      row.push(0);
+    }
+    // Pushes the current row, which now has n zeroes in it, to the array
+    newArray.push(row);
+  }
+  return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
+
+//21
+
+function myFunc() {
+  for (let i = 1; i <= 4; i += 2) {
+    console.log('Still going!');
+  }
+}
