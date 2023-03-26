5a65fb7a15ed6ff86482dc04f41b2857782d0bab
Serhii <875dfsb6435278785@gmail.com>
2023-03-26 22:37:49 +0200

    it-incubator

--- codewars/script.js
+++ codewars/script.js
@@ -1484,3 +1484,18 @@ function updateLight(current) {
 
 //139
 
+function check(a,x){
+  return a.includes(x);
+ };
+
+//140
+
+function apple(x){
+  if((x ** 2) > 1000){
+    return "It's hotter than the sun!!"
+  }
+    return "Help yourself to a honeycomb Yorkie for the glovebox."
+  }
+
+  //141
+
