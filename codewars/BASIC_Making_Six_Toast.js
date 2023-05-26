5a9767a395902326ce3eccb9ab5875a2bde82262
Serhii <875dfsb6435278785@gmail.com>
2023-05-26 23:06:42 +0200

    codewars

---
+++ codewars/BASIC_Making_Six_Toast.js
@@ -0,0 +1,7 @@
+/*
+You are going to make toast fast, you think that you should make multiple pieces of toasts and once. So, you try to make 6 pieces of toast.*/
+
+///////-------------------------------------------------------//////////
+function sixToast(num) {
+    var a = 6; return Math.abs(num-6); // you code here
+}
---
+++ codewars/Convert_a_Number_to_a_String!.js
@@ -0,0 +1,11 @@
+/*We need a function that can transform a number (integer) into a string.
+
+    What ways of achieving this do you know?*/
+
+    ///------------------------------------------------------///
+
+function numberToString(num) {
+    return num = String(num)// Return a string of the number here!
+}
+
+
