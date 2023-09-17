a2eb428f4a1f424c2d2fb778d3acd89bb41591ea
Serhii <875dfsb6435278785@gmail.com>
2023-09-17 16:51:08 +0200

    codevars_9_17_23

---
+++ codewars/Grasshopper-Terminal_game_combat_function.js
@@ -0,0 +1,9 @@
+/*
+Exercise
+
+Create a combat function that takes the player's current health and the amount of damage recieved, ' +
+'and returns the player's new health. Health can't be less than 0.
+
+Solution*/
+
+combat = (health, damage) => damage > health ? 0 : health - damage
